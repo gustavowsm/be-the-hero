cdecdfc64f63faa41997670262f05b290aa05ab2
Gustavo <39592614+gustavowsm@users.noreply.github.com>
2020-03-31 15:58:33 -0300

    front end

---
+++ front end/src/App.js
@@ -0,0 +1,20 @@
+import React, {useState } from 'react';
+
+import './global.css';
+
+import Routes from './routes';
+
+
+function App() {
+
+
+  return (
+
+<Routes/>
+    
+  );
+}
+
+
+
+export default App;
